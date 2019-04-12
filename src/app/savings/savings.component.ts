cac114a7dc60025e96a792dbb31384f8d0308c4a
paper-reserve <re.serve.pa.per.10@gmail.com>
2019-04-12 09:05:31 +0530

    v4.0.1 lic info added

--- src/app/savings/savings.component.ts
+++ src/app/savings/savings.component.ts
@@ -10,7 +10,9 @@ import { DataService } from "../services/data.service";
 export class SavingsComponent implements OnInit {
   result;
   instalments = [];
+  lics = [];
   headers = [];
+  licHeaders = [];
   loading = false;
   constructor(private data: DataService) {}
 
@@ -22,6 +24,22 @@ export class SavingsComponent implements OnInit {
       this.instalments = this.result.instalments;
       this.headers = this.instalments.shift();
       this.loading = false;
+    });
+  }
+  getLic() {
+    this.loading = true;
+    this.data.getSheetInfo("LIC").subscribe(data => {
+      this.result = data;
+      this.loading = false;
+      this.licHeaders = this.result.lic.shift();
+      this.lics = [];
+      this.result.lic.forEach(function(item, i) {
+        let temp = {};
+        item.forEach(function(col, j) {
+          temp[this.licHeaders[j]] = col;
+        }, this);
+        this.lics.push(temp);
+      }, this);
     });
   }
 }
